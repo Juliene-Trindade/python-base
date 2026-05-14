66dd92f57fd34347202e578d39d8a2497d94d179
Juliene Trindade <jtsm_juliene@hotmail.com>
2026-05-14 20:31:54 -0300

    RotatingFileHandler logs

--- logs.py
+++ logs.py
@@ -2,20 +2,27 @@
 
 import os
 import logging
+from logging import handlers
 
 # BOILERPLATE
 # TODO: usar função
 # TODO: usar lib (loguru)
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.Logger("Juliene", log_level)
-ch = logging.StreamHandler() # CONSOLE/TERMINAL/STERR
-ch.setLevel(log_level)
+#ch = logging.StreamHandler() # CONSOLE/TERMINAL/STERR
+#ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+    "meulog.log", 
+    maxBytes=10**6, 
+    backupCount=10,
+)
+fh.setLevel(log_level)
 fmt = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s'
     'l:%(lineno)s f:%(filename)s: %(message)s'
 )
-ch.setFormatter(fmt)
-log.addHandler(ch)
+fh.setFormatter(fmt)
+log.addHandler(fh)
 
 """
 log.debug("[DEBUG] Mensagem pro dev, qe, sysadmin.")
